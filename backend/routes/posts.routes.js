e4265b9b8d53be616d84e2a0e629deee2394919b
Anna Kijania <kijania.anna@gmail.com>
2021-01-25 14:18:23 +0100

    Store new new post in db

--- backend/routes/posts.routes.js
+++ backend/routes/posts.routes.js
@@ -1,31 +1,84 @@
 const express = require('express');
 const router = express.Router();
+const Joi = require('joi');
 
 const Post = require('../models/post.model');
 
 router.get('/posts', async (req, res) => {
   try {
-    const result = await Post
-      .find({status: 'published'})
+    const result = await Post.find({ status: 'published' })
       .select('_id author created title photo status')
-      .sort({created: -1});
-    if(!result) res.status(404).json({ post: 'Not found' });
+      .sort({ created: -1 });
+    if (!result) res.status(404).json({ post: 'Not found' });
     else res.json(result);
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).json(err);
   }
 });
 
 router.get('/posts/:id', async (req, res) => {
   try {
-    const result = await Post
-      .findById(req.params.id);
-    if(!result) res.status(404).json({ post: 'Not found' });
+    const result = await Post.findById(req.params.id);
+    if (!result) res.status(404).json({ post: 'Not found' });
     else res.json(result);
-  }
-  catch(err) {
+  } catch (err) {
     res.status(500).json(err);
+  }
+});
+
+router.post('/posts', async (req, res) => {
+  console.log(res.body);
+  const schema = Joi.object({
+    author: Joi.string().email({
+      minDomainSegments: 2,
+      tlds: { allow: ['com', 'net', 'pl'] },
+    }),
+    created: Joi.date().iso().required(),
+    updated: Joi.date().iso().required(),
+    status: Joi.string().valid('draft', 'published', 'finished').required(),
+    title: Joi.string().alphanum().min(3).max(10).required(),
+    text: Joi.string().alphanum().min(3).max(50).required(),       
+  });
+  try {
+    const { author,
+      created,
+      updated,
+      status,
+      title,
+      text,
+      photo,
+      price,
+      phone,
+      location } = req.body;
+    console.log(created, status);
+    if (!author || !created || !updated || !status || !title || !text)
+      throw new Error('Invalid data');
+    else {
+      const value = await schema.validateAsync({
+        author: author,
+        created: created,
+        updated: updated,
+        status: status,
+        title: title,
+        text: text,
+      });
+      const newPost = new Post({
+        author: author,
+        created: created,
+        updated: updated,
+        status: status,
+        title: title,
+        text: text,
+        photo: photo,
+        price: price,
+        phone: phone,
+        location: location,
+      });
+
+      await newPost.save();
+      res.json({ message: 'OK' });
+    }} catch (err) {
+    res.status(500).json({ message: err });
   }
 });
 
